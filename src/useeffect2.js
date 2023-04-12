bd1e0ab0a697767566e6e9596ab31c4f5cbcdf10
Cooper <congqin.chen@ryerson.ca>
2023-04-12 18:29:56 -0400

    changes

---
+++ src/useeffect2.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+
+export default function Ueffect2(){
+    const [windowWidth, setwindowWidth] = useState(window.innerWidth)
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
