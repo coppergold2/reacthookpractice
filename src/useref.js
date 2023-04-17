2f26f1a49ddf538aa2fa8a50139d83b107f906b0
Cooper <congqin.chen@ryerson.ca>
2023-04-17 08:51:22 -0400

    progess

--- src/useref.js
+++ src/useref.js
@@ -2,13 +2,15 @@ import React, { useState, useRef, useEffect } from 'react'
 
 export default function Uref(){
     const [name, setName] = useState('')
-
+    const renderCount = useRef(1);
+    useEffect (() => {
+        renderCount.current = renderCount.current+1;
+    })
     return (
         <>
         <input value ={name} onChange = {e => setName(e.target.value)}/>
-
         <div>My name is {name} </div>
-        <div> I rendered {x} times</div>
+        <div> I rendered {renderCount.current} times</div>
         </>
     )
 }
